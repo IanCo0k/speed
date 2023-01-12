b97ce2316663987890fca19b4b533795a70b0a69
Ian Cook <ianscook19@gmail.com>
2023-01-12 17:46:21 -0500

    adding contact elements on the bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faPhone, faEnvelope, faE } from '@fortawesome/free-solid-svg-icons';
 import './Footer.css';
 
 export default function Footer() {
   return (
     <div className="footer">
         <div className="socials">
-            <FontAwesomeIcon className='icon' icon={faInstagram}/>
+            <a href="https://www.instagram.com/westside_speed/"><FontAwesomeIcon className='icon' icon={faInstagram}/></a>
+            <a href="tel:616-498-9795"><FontAwesomeIcon className='icon' icon={faPhone}/></a>
+            <a href="mailto:haddadeli89@gmail.com"><FontAwesomeIcon className='icon' icon={faEnvelope}/></a>
         </div>
         <h3>Westside Speed</h3>
     </div>
