a43abc07660cb3363e12fac9ba2711afbca0a5dc
Ian Cook <ianscook19@gmail.com>
2023-01-12 17:37:47 -0500

    fixing import bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Contact from './components/Pages/Contact';
 import Schedule from './components/Pages/Schedule';
 import About from './components/Pages/About';
 import './app.css';
@@ -11,7 +10,6 @@ export default function App() {
     <div>
         <Routes>
             <Route exact path='/' element={<Home/>}/>
-            <Route path='/contact' element={<Contact/>}/>
             <Route path='/schedule' element={<Schedule/>}/>
             <Route path='/about' element={<About/>}/>
         </Routes>
